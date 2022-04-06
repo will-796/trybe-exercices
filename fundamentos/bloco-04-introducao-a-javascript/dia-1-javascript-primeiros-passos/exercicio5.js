38009e180c3a4b6da86ece9cf454ccae1010cb73
Willian Portela <willianportela1@gmail.com>
2022-04-06 18:09:07 -0300

    exercicio 5

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 70;
+let c = 100;
+
+if ((a, b, c < 0)) {
+  console.log("Todos os valores devem ser positivos");
+} else if (a + b + c == 180) {
+  console.log(true , "Parabéns você tem um triângulo");
+} else {
+  console.log(false, "Infelizmente você não tem um triângulo :c");
+}
