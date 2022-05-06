10ec05cc66a89e8712580e03609e84e45a226f1a
Willian Portela <willianportela1@gmail.com>
2022-05-06 15:38:58 -0300

    exercicio 2 parte 1

---
+++ fundamentos/bloco-07-javascript-ES6/dia-1-let-const-arrowFunctions-templateLiterals/parte1/script1.js
@@ -0,0 +1,6 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const oddsAndEvensSorted = oddsAndEvens.sort()
+
+console.log(oddsAndEvensSorted); // será necessário alterar essa linha 😉
