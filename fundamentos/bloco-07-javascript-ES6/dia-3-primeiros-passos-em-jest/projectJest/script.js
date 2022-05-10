5ffb34cbbab771641ebd24e3693a5eb098a34e5b
Willian Portela <willianportela1@gmail.com>
2022-05-10 20:53:31 -0300

    exercicio 4

--- fundamentos/bloco-07-javascript-ES6/dia-3-primeiros-passos-em-jest/projectJest/script.js
+++ fundamentos/bloco-07-javascript-ES6/dia-3-primeiros-passos-em-jest/projectJest/script.js
@@ -1,6 +1,6 @@
 function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("parameters must be numbers");
   }
 
   return a + b;
@@ -17,15 +17,64 @@ function myRemove(arr, item) {
 }
 
 function myFizzBuzz(num) {
-  if (typeof num !== 'number') return false;
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
+  if (typeof num !== "number") return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
   return num;
+}
+
+function encode(string) {
+  // seu código aqui
+  let newString = "";
+  for (const char of string) {
+    if (char === "a") {
+      newString += "1";
+    } else if (char === "e") {
+      newString += "2";
+    } else if (char === "i") {
+      newString += "3";
+    } else if (char === "o") {
+      newString += "4";
+    } else if (char === "u") {
+      newString += "5";
+    } else {
+      newString += char;
+    }
+  }
+  return newString;
+}
+
+function decode(string) {
+  // seu código aqui
+  let newString = "";
+  string.replace()
+  for (const char of string) {
+    if (char === "1") {
+      newString += "a";
+    } else if (char === "2") {
+      newString += "e";
+    } else if (char === "3") {
+      newString += "i";
+    } else if (char === "4") {
+      newString += "o";
+    } else if (char === "5") {
+      newString += "u";
+    } else {
+      newString += char;
+    }
+  }
+  return newString
 }
 
 // implemente seus testes aqui
 
 // implemente seus testes aqui
 
-module.exports = {sum, myRemove, myFizzBuzz}
+module.exports = {
+  sum,
+  myRemove,
+  myFizzBuzz,
+  encode,
+  decode,
+};
--- fundamentos/bloco-07-javascript-ES6/dia-3-primeiros-passos-em-jest/projectJest/tests/myFizzBuzz.test.js
+++ fundamentos/bloco-07-javascript-ES6/dia-3-primeiros-passos-em-jest/projectJest/tests/myFizzBuzz.test.js
@@ -2,12 +2,15 @@ const { myFizzBuzz } = require("../script");
 
 describe("test myFizzBuzz", () => {
   it("input 15 and return fizzbuzz", () => {
-    expect(myFizzBuzz(15)).toMatch('fizzbuzz');
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
   });
   it("input 9 and return buzz", () => {
-    expect(myFizzBuzz(9)).toMatch('fizz');
+    expect(myFizzBuzz(9)).toBe('fizz');
   });
   it("input 10 and return fizzbuzz", () => {
-    expect(myFizzBuzz(10)).toMatch('buzz');
+    expect(myFizzBuzz(10)).toBe('buzz');
+  });
+  it("input string and return false", () => {
+    expect(myFizzBuzz('10')).toBeFalsy();
   });
 });
